dfc7c682c00390303b7637fc4cc054f4f75d32dd
Sierra <68665290+spo0pyGD@users.noreply.github.com>
2023-01-06 22:36:03 -0500

    MIT License

--- FoodPickerV2/Main.cpp
+++ FoodPickerV2/Main.cpp
@@ -1,3 +1,26 @@
+/*
+* Copyright (c) 2012-2022 Scott Chacon and others
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 #include <iostream>
 #include <vector>
 #include <string>
@@ -98,10 +121,10 @@ void MainMenu()
 {
 	int menuChoice = 0;
 
-#ifndef NDEBUG
+//#ifndef NDEBUG
 	std::string filepath = "foodcategories.txt";
-#endif
-	std::string filepath = "../SaveData/foodcategories.txt";
+//#endif
+	//std::string filepath = "../SaveData/foodcategories.txt";
 
 	// Resize window
 	RECT rect; // stores console's current dimensions
@@ -367,10 +390,10 @@ void TextPrompt(const char *_msg, char *&_text)
 // Save categories to a new text file
 void SaveList()
 {
-#ifndef NDEBUG
+//#ifndef NDEBUG
 	std::string fileName("foodcategories.txt");
-#endif
-	std::string fileName("../SaveData/foodcategories.txt");
+//#endif
+	//std::string fileName("../SaveData/foodcategories.txt");
 	std::string line;
 	std::ofstream file;
 
